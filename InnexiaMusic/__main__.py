87e15dc0c42c758d355fb3bec8d7a2c8fd133843
abdsanji <89727732+abdsanji@users.noreply.github.com>
2021-09-26 12:57:42 +0800

    Update __main__.py

--- InnexiaMusic/__main__.py
+++ InnexiaMusic/__main__.py
@@ -17,5 +17,18 @@ bot = Bot(
     plugins=dict(root="plugins"),
 )
 
+
+START_TEXT = """
+Hello {}, I am Telegram calculator bot 
+ Send me /calc or /claculator and See what can I do!.
+My creator [@Shoto_GirlFriend_777](https://t.me/Shoto_GirlFriend_777/604)
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Join my group 💗', url='https://t.me/animefan_club777'),
+        InlineKeyboardButton('Channel 📢', url='https://t.me/moviesebseriesAnimes')
+        ]]
+    )
+
 bot.start()
 run()
